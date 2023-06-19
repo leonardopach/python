55b22097705c0f0fcc18dedbb1abe011ba438e06
Leonardo Pacheco Dos Santos <l.pacheco@globant.com>
2023-06-19 13:08:48 -0300

    fundamentose

---
+++ fundamentos/primeiroPrograma.py
@@ -0,0 +1,4 @@
+print("Primeiro programa")
+a = 1+2
+print(a)
+help(print)
---
+++ fundamentos/tiposBasicos.py
@@ -0,0 +1,12 @@
+print(True)
+print(False)
+print(1.2 + 1)
+print("Aqui eu falo normal")
+print('Funciona de boas')
+print('Voce e ' + 3 * 'muito ' + 'legal')
+print([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+print({'nome': 'Pedro', "idade": 12})
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+dicionario = {'nome': 'Pedro', "idade": 12}
+print(dicionario['nome'])
