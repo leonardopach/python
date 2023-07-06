16cb34093dc8286a5fc32be6b4c0da24658e4075
leonardopach <pachecodossantos17@gmail.com>
2023-07-06 07:29:22 -0300

    sobreposição de metodos super

--- python_luiz_otavio/python_POO/sobreposicao_metodos_super.py
+++ python_luiz_otavio/python_POO/sobreposicao_metodos_super.py
@@ -1,47 +1,64 @@
-class MinhaString(str):
-    def upper(self):
-        print("Chamando upper")
-        retorno = super(MinhaString, self).upper()
-        return retorno
-
-    def capitalize(self):
-        print("Chamando capitalize")
-        retorno = super(MinhaString, self).capitalize()
-        return retorno
-
-
-string = MinhaString("leonardo")
-print(string.upper())
-print(string.capitalize())
+# super() e a sobreposição de membros - Python Orientado a Objetos
+# Classe principal (Pessoa)
+#   -> super class, base class, parent class
+# Classes filhas (Cliente)
+#   -> sub class, child class, derived class
+# class MinhaString(str):
+#     def upper(self):
+#         print('CHAMOU UPPER')
+#         retorno = super(MinhaString, self).upper()
+#         print('DEPOIS DO UPPER')
+#         return retorno
 
 
-class A:
-    atributo_a = "valor A"
+# string = MinhaString('Luiz')
+# print(string.upper())
+
+class A(object):
+    atributo_a = 'valor a'
+
+    def __init__(self, atributo):
+        self.atributo = atributo
 
     def metodo(self):
-        print("A")
+        print('A')
 
 
 class B(A):
-    atributo_b = "valor B"
+    atributo_b = 'valor b'
+
+    def __init__(self, atributo, outra_coisa):
+        super().__init__(atributo)
+        self.outra_coisa = outra_coisa
 
     def metodo(self):
-        super().metodo()
-        print("B")
+        print('B')
 
 
 class C(B):
-    atributo_c = "valor C"
+    atributo_c = 'valor c'
+
+    def __init__(self, *args, **kwargs):
+        # print('EI, burlei o sistema.')
+        super().__init__(*args, **kwargs)
 
     def metodo(self):
-        super().metodo()
-        print("C")
+        # super().metodo()  # B
+        # super(B, self).metodo()  # A
+        # super(A, self).metodo()  # object
+        A.metodo(self)
+        B.metodo(self)
+        print('C')
 
 
-atributo = C()
-
-print(atributo.atributo_a)
-print(atributo.atributo_b)
-print(atributo.atributo_c)
-atributo.metodo()
-print(C.mro())
+# print(C.mro())
+# print(B.mro())
+# print(A.mro())
+c = C('Atributo', 'Qualquer')
+# print(c.atributo)
+# print(c.outra_coisa)
+c.metodo()
+# print(c.atributo_a)
+# print(c.atributo_b)
+# print(c.atributo_c)
+# c.metodo()
