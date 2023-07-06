2e65b8ca7ddb31081b5e026afaa1480133a29a62
leonardopach <pachecodossantos17@gmail.com>
2023-07-06 08:53:51 -0300

    Mixins abstração e log

---
+++ python_luiz_otavio/python_POO/mixins/eletronico.py
@@ -0,0 +1,29 @@
+from log import LogFileMixin
+
+
+class Eletronico:
+    def __init__(self, nome) -> None:
+        self._nome = nome
+        self._ligado = False
+
+    def ligar(self):
+        if not self._ligado:
+            self._ligado = True
+
+    def desligar(self):
+        if self._ligado:
+            self._ligado = False
+
+
+class Smartphone(Eletronico, LogFileMixin):
+    def ligar(self):
+        super().ligar()
+
+        if self._ligado:
+            self.log_success(f"{self._nome} está ligado")
+
+    def desligar(self):
+        super().desligar()
+
+        if not self._ligado:
+            self.log_success(f"{self._nome} está desligado")
---
+++ python_luiz_otavio/python_POO/mixins/main.py
@@ -0,0 +1,9 @@
+from eletronico import Smartphone
+
+
+galaxy_s = Smartphone("Galaxy S")
+iphone = Smartphone("iphone")
+
+galaxy_s.ligar()
+galaxy_s.desligar()
+iphone.desligar()
