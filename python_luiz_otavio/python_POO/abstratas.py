7891ec197605805d0f52dda16c409809dc4b21f4
leonardopach <pachecodossantos17@gmail.com>
2023-07-06 21:24:15 -0300

    abstração

---
+++ python_luiz_otavio/python_POO/abstratas.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class Log(ABC):
+    @abstractmethod
+    def _log(self, msg): ...
+
+    def log_error(self, msg):
+        return self._log(f"Error: {msg}")
+
+    def log_success(self, msg):
+        return self._log(f"success: {msg}")
+
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+
+a = LogPrintMixin()
+a.log_error("Erro")
