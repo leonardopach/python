e03870332a2829d1fe7ea4b7ff55008252f9e3f2
leonardopach <pachecodossantos17@gmail.com>
2023-07-06 21:25:02 -0300

    Criação de exceções

---
+++ python_luiz_otavio/python_POO/criando_exceptions.py
@@ -0,0 +1,24 @@
+class MeuErro(Exception):
+    ...
+
+
+class OutroErro(Exception):
+    ...
+
+
+def levantar():
+    expection_ = MeuErro("A", "B", "B")
+    expection_.add_note("Olha a nota 1")
+    expection_.add_note("Você errou isso")
+    raise expection_
+
+
+try:
+    1/0
+    levantar()
+except (MeuErro, ZeroDivisionError) as erro:
+    print(erro.__class__.__name__)
+    print(erro)
+    print()
+    Exception_ = OutroErro("Vou lançar de novo")
+    raise Exception_ from erro
