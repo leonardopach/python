4fd8a3b1c6d670880abc0b590480ccbc49b66867
leonardopach <pachecodossantos17@gmail.com>
2023-07-12 07:39:59 -0300

    openpyxl

---
+++ python_luiz_otavio/modulo_python/reading_openpyxl.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from openpyxl import Workbook, load_workbook
+from openpyxl.cell import Cell
+from openpyxl.worksheet.worksheet import Worksheet
+
+ROOT_FOLDER = Path(__file__).parent
+WORKBOOK_PATH = ROOT_FOLDER / 'workbook.xlsx'
+
+# carregando planilha
+workbook: Workbook = load_workbook(WORKBOOK_PATH)
+# worksheet: Worksheet = workbook.active  # type: ignore
+
+# Selecionar Planilha
+worksheet: Worksheet = workbook["Minha Planilha"]  # type: ignore
+
+row: tuple[Cell]
+for row in worksheet.iter_rows():
+    for cell in row:
+        print(cell.value, end='\t')
+    print()
+
+worksheet['b3'].value = 15
+
+workbook.save(WORKBOOK_PATH)
