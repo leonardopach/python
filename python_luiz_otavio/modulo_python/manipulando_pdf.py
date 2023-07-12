a530ff3c37c6875711570ca30496189a3aeca073
leonardopach <pachecodossantos17@gmail.com>
2023-07-11 22:05:24 -0300

    Manipulando arquivo pdf

--- python_luiz_otavio/modulo_python/manipulando_pdf.py
+++ python_luiz_otavio/modulo_python/manipulando_pdf.py
@@ -2,7 +2,7 @@
 # pip install pypdf2
 from pathlib import Path
 
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfWriter
 
 PASTA_RAIZ = Path(__file__).parent
 PASTA_ORIGINAIS = PASTA_RAIZ / 'pdf'
@@ -18,4 +18,16 @@ reader = PdfReader(RELATORIO_BACEN)
 #     print(page)
 
 page0 = reader.pages[0]
+imagem0 = page0.images[2]
 print(page0.extract_text())
+print(imagem0)
+
+# with open(PASTA_NOVA / imagem0.name, 'wb') as fp:
+#     fp.write(imagem0.data)
+
+
+writer = PdfWriter()
+with open(PASTA_NOVA / 'renata.pdf', 'wb') as fp:
+    for page in reader.pages:
+        writer.add_page(page)
+    writer.write(fp)  # type: ignore
