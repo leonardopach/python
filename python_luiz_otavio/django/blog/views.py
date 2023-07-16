57e020759108b94f50d5ea47648996bef7e8d888
leonardopach <pachecodossantos17@gmail.com>
2023-07-16 15:46:10 -0300

    DJANGO

---
+++ python_luiz_otavio/django/blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+# Create your views here.
+
+
+def blog(request):
+    return render(
+        request,
+        "blog/index.html"
+    )
+
+
+def exemplo(request):
+    return render(
+        request,
+        "blog/exemplo.html"
+    )
