b1cab0f9d5f0db1cf18e2e4b3bbb796394611c2d
Leonardo Pacheco Dos Santos <l.pacheco@globant.com>
2023-07-19 08:57:47 -0300

    Projeto agenda

--- python_luiz_otavio/agenda/contact/admin.py
+++ python_luiz_otavio/agenda/contact/admin.py
@@ -1,7 +1,20 @@
+from contact import models
 from django.contrib import admin
-from contact.models import Contact
 
 # Register your models here.
-@admin.register(Contact)
+
+
+@admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'category',
+    ordering = 'id',
+    list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 2
+    list_display_links = 'id', 'first_name',
+
+
+@admin.register(models.Category)
+class Category(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    ordering = 'id',
